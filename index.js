109f66bd36a0fb274deb8b5bd0b1bb4b3fb20261
Priscilla-22 <priscillamw123@gmail.com>
2023-12-08 23:44:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solutions in this file
+
+//practice solution
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts); 
+
+//ASSIGNMENT 1
+ const names = ["Tom", "Jerry", "Simpsons", "Ricky"];
+const eventName = "swimmingGala";
+
+function wrapGifts(names, eventName){
+    const messages = [];
+    for(i = 0; i < names.length; i++){
+    const congrats = `Congratulation ${names[i]}, you won the ${eventName}!`;
+    messages.push(congrats);
+}
+ return messages;
+}
+const finalMessage = wrapGifts(names, eventName);
+console.log(finalMessage); 
+
+//ASSIGNMENT 2
+const number = 20;
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+  return number;
+}
+countDown(number);
